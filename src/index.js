4a144b78a2943a74c9d1fc32df397f8715e0068d
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-15 21:23:59 +0700

    Feature: connectedmodules to index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import benefitsSlider from './modules/benefitsSlider.js'
+import servicesSlider from './modules/servicesSlider.js'
+import documents from './modules/documents.js'
+import calculater from './modules/calculater.js'
+import timer from './modules/timer.js'
+import sendForm from './modules/sendForm.js'
+import headerModal from './modules/headerModal.js'
+import servicesModal from './modules/servicesModal.js'
+import scroll from './modules/scroll.js'
+
+benefitsSlider()
+servicesSlider()
+documents()
+calculater()
+timer()
+sendForm()
+scroll()
+headerModal()
+servicesModal()
---
+++ src/modules/calculater.js
@@ -0,0 +1,5 @@
+const calculater = () => {
+
+}
+
+export default calculater

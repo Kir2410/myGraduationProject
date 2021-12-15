4b732185a83ebbdb6d9bde0d960bbe3eebcdb569
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-15 22:42:10 +0700

    Feature (servicesModal.js): add modal to click on link

--- src/modules/servicesModal.js
+++ src/modules/servicesModal.js
@@ -6,6 +6,7 @@ const servicesModal = () => {
     const modal = document.querySelector('.overlay');
     const modalContent = document.querySelector('.services-modal ');
     const buttons = document.querySelectorAll('.service-button');
+    const link = document.querySelector('.fadeInUp').querySelector('a.fancyboxModal');
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
@@ -22,6 +23,20 @@ const servicesModal = () => {
             })
         })
     });
+
+    link.addEventListener('click', (e) => {
+        animate({
+            duration: 500,
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                modal.style.display = 'block';
+                modalContent.style.display = 'block';
+                modalContent.style.opacity = progress;
+            }
+        })
+    })
 
     modalContent.addEventListener('click', (e) => {
         if (e.target.classList.contains('services-modal__close')) {

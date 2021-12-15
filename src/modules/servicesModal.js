ea4805ad6e544dfebb2b402dd14ea5cfb707dd41
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-15 22:07:01 +0700

    Feature (servicesModal.js): create services modal

--- src/modules/servicesModal.js
+++ src/modules/servicesModal.js
@@ -1,5 +1,34 @@
-const servicesModal = () => {
+import {
+    animate
+} from "./helpers"
 
+const servicesModal = () => {
+    const modal = document.querySelector('.overlay');
+    const modalContent = document.querySelector('.services-modal ');
+    const buttons = document.querySelectorAll('.service-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            animate({
+                duration: 500,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    modal.style.display = 'block';
+                    modalContent.style.display = 'block';
+                    modalContent.style.opacity = progress;
+                }
+            })
+        })
+    });
+
+    modalContent.addEventListener('click', (e) => {
+        if (e.target.classList.contains('services-modal__close')) {
+            modal.style.display = 'none';
+            modalContent.style.display = 'none';
+        }
+    })
 }
 
 export default servicesModal

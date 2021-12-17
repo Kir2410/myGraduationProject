87af47f78b1c2d42fa3cdfc247b7c382e824bc26
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-17 09:28:33 +0700

    Feature (documents.js): start work in document block

--- src/modules/documents.js
+++ src/modules/documents.js
@@ -1,20 +1,44 @@
+import {
+    animate
+} from "./helpers"
+
 const documents = () => {
     const docs = document.querySelectorAll('.document-overlay')
+    const modal = document.querySelector('.overlay ');
+    const modalContent = document.querySelector('.header-modal ');
 
     docs.forEach((doc) => {
         doc.addEventListener('mouseenter', () => {
             doc.style.opacity = 1
-            doc.style.width = '80%'
-            doc.style.left = '20px'
-            console.log('in')
+            // doc.style.width = '80%'
+            // doc.style.left = '20px'
         })
         doc.addEventListener('mouseleave', () => {
             doc.style.opacity = 0
-            console.log('leave')
         })
-        doc.addEventListener('mouseleave', () => {
-            doc.style.opacity = 0
-            console.log('leave')
+        doc.addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log(e.target.closest('a').pathname)
+
+            modalContent.innerHTML = `<img src=${e.target.closest('a').pathname} height=400px>`
+            console.log(modalContent)
+            // let img = e.target.closest('a').children[0]
+            // console.dir(img)
+            // img.src = e.target.closest('a').pathname
+            // img.style.display = 'block'
+
+
+            animate({
+                duration: 500,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    modal.style.display = 'block';
+                    modalContent.style.display = 'block';
+                    modalContent.style.opacity = progress;
+                }
+            })
         })
     })
 }

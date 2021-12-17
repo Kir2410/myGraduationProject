a54f63215bb96d7aeb430a6b467615561858a7d0
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-17 13:59:51 +0700

    Feature (scroll.js): create autoscroll function

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,25 @@
 const scroll = () => {
+    const btn = document.querySelector('.smooth-scroll')
 
+    btn.style.cursor = "pointer";
+    btn.style.display = 'none'
+
+    document.addEventListener('scroll', () => {
+        console.log((scrollY > 500))
+        if ((scrollY > 500)) {
+            btn.style.display = 'block'
+        } else {
+            btn.style.display = 'none'
+        }
+    })
+
+    btn.addEventListener('click', () => {
+        scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+    })
 }
 
 export default scroll

b15c6002620ff440c63d28a22b5b3e20d4eedbd4
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-22 11:21:57 +0700

    Fix (validator.js): fixed validator for input form

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -8,6 +8,7 @@ const validator = () => {
     document.querySelectorAll('input[name="phone"]').forEach(item => {
         item.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^0-9\+]/, "");
+            e.target.value = e.target.value.replace(/[0-9]{17}/, "");
         })
     });
 }
